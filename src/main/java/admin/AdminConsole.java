275d7d02336f629a88944eb212670859525263b6
Filip Rydzi <rydzi.filip@gmail.com>
2015-01-05 15:14:19 +0100

    AdminConsole and ComputationRequestInfo implemented

--- src/main/java/admin/AdminConsole.java
+++ src/main/java/admin/AdminConsole.java
@@ -4,25 +4,36 @@ import controller.IAdminConsole;
 import model.ComputationRequestInfo;
 import util.Config;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 import java.security.Key;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
- * Please note that this class is not needed for Lab 1, but will later be
- * used in Lab 2. Hence, you do not have to implement it for the first
- * submission.
+ * Please note that this class is not needed for Lab 1, but will later be used
+ * in Lab 2. Hence, you do not have to implement it for the first submission.
  */
-public class AdminConsole implements IAdminConsole, Runnable {
-
+public class AdminConsole implements IAdminConsole, Runnable, INotificationCallback {
+	
 	private String componentName;
 	private Config config;
 	private InputStream userRequestStream;
 	private PrintStream userResponseStream;
 
+	private IAdminConsole remoteInterface;
+	
+	private Registry registry;
+	
 	/**
 	 * @param componentName
 	 *            the name of the component - represented in the prompt
@@ -39,32 +50,132 @@ public class AdminConsole implements IAdminConsole, Runnable {
 		this.config = config;
 		this.userRequestStream = userRequestStream;
 		this.userResponseStream = userResponseStream;
-
 		// TODO
+	}
+
+	private boolean bindRemoteInterface() {
+		// obtain registry that was created by the server
+		try {
+			registry = LocateRegistry.getRegistry(
+					config.getString("controller.host"),
+					config.getInt("controller.rmi.port"));
+			// look for the bound server remote-object implementing the
+			// IAdminConsole
+			// interface
+			remoteInterface = (IAdminConsole) registry.lookup(config
+					.getString("binding.name"));
+			
+
+		} catch (RemoteException e1) {
+			return false;
+		} catch (NotBoundException e1) {
+			return false;
+		} 
+		return true;
 	}
 
 	@Override
 	public void run() {
-		// TODO
+
+		bindRemoteInterface();
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+				userRequestStream));
+
+		String command = "";
+
+		try {
+			while ((command = reader.readLine()) != null) {
+
+				try {
+					if (command.startsWith("!getLogs")) {
+						if(remoteInterface == null) {
+							throw new RemoteException("Connection wasn't established");
+						}
+						String logs = formatLogs(getLogs());
+						userResponseStream.println(logs);
+
+					}
+					else if (command.startsWith("!statistics")) {
+						if(remoteInterface == null) {
+							throw new RemoteException("Connection wasn't established");
+						}
+						String statistics = formatStatistics(statistics());
+						userResponseStream.println(statistics);
+					}
+					else if (command.startsWith("!subscribe")) {
+						if(remoteInterface == null) {
+							throw new RemoteException("Connection wasn't established");
+						}
+						userResponseStream.println(subscribe("username", 500, this));
+					}
+
+				} catch (RemoteException e ) {
+					if(bindRemoteInterface()) { //if cloudcontroller was down (connection with adminconsole get lost) and was started again
+						String resp = "";
+						if(command.startsWith("!getLogs")) {
+							resp = formatLogs(getLogs());
+						} else if (command.startsWith("!statistics")) {
+							resp = formatStatistics(statistics());
+						} else if (command.startsWith("!subscribe")) {
+							resp = Boolean.toString(subscribe("username", 500, this));
+						}
+						userResponseStream.println(resp);
+					} else {
+						userResponseStream.println("Problem invoking remote method.");
+					}
+				}
+
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	private String formatLogs(List<ComputationRequestInfo> logs) {
+		String logsString = "";
+		if(logs == null || logs.size() == 0){
+			return "There are no nodes alive.";
+		}
+		for(ComputationRequestInfo cr : logs){
+			logsString = logsString + cr.toString() + '\n';
+		}
+		
+		return logsString;
+	}
+	
+	private String formatStatistics(LinkedHashMap<Character, Long> operators) {
+		String statistics = "";
+		if(operators == null || operators.size() == 0) {
+			return "No operator was used";
+		}
+		for (Map.Entry<Character, Long> entry : operators.entrySet()) {
+			Character key = entry.getKey();
+			Long value = entry.getValue();
+			statistics = statistics + key + " " + value + '\n';
+		}
+		return statistics;
 	}
 
 	@Override
 	public boolean subscribe(String username, int credits,
 			INotificationCallback callback) throws RemoteException {
-		// TODO Auto-generated method stub
-		return false;
+		
+		// create a remote object of INotificationCallback object
+		INotificationCallback notCallback = (INotificationCallback) UnicastRemoteObject.exportObject(callback, 0);
+		
+		return remoteInterface.subscribe("username", 500, notCallback);
 	}
 
 	@Override
 	public List<ComputationRequestInfo> getLogs() throws RemoteException {
-		// TODO Auto-generated method stub
-		return null;
+		return remoteInterface.getLogs();
 	}
 
 	@Override
 	public LinkedHashMap<Character, Long> statistics() throws RemoteException {
-		// TODO Auto-generated method stub
-		return null;
+		return remoteInterface.statistics();
 	}
 
 	@Override
@@ -87,6 +198,13 @@ public class AdminConsole implements IAdminConsole, Runnable {
 	public static void main(String[] args) {
 		AdminConsole adminConsole = new AdminConsole(args[0], new Config(
 				"admin"), System.in, System.out);
-		// TODO: start the admin console
+		new Thread(adminConsole).start();
 	}
+
+	@Override
+	public void notify(String username, int credits) throws RemoteException {
+		userResponseStream.println(username + " has "+credits);
+		
+	}
+	
 }
--- src/main/java/model/ComputationRequestInfo.java
+++ src/main/java/model/ComputationRequestInfo.java
@@ -1,10 +1,76 @@
 package model;
 
+import java.io.Serializable;
+
 /**
  * Please note that this class is not needed for Lab 1, but will later be
  * used in Lab 2. Hence, you do not have to implement it for the first
  * submission.
  */
-public class ComputationRequestInfo {
-	// TODO: implement
+public class ComputationRequestInfo implements Serializable, Comparable<ComputationRequestInfo>{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2420582513780382631L;
+	
+	private String nodeName;
+	private String operationRequest;
+	private String operationResult;
+	private String operationTimestamp;
+	
+	
+	
+	public ComputationRequestInfo(String nodeName, String operationRequest,
+			String operationResult, String operationTimestamp) {
+		super();
+		this.nodeName = nodeName;
+		this.operationRequest = operationRequest;
+		this.operationResult = operationResult;
+		this.operationTimestamp = operationTimestamp;
+	}
+	
+	
+	public String getNodeName() {
+		return nodeName;
+	}
+	public void setNodeName(String nodeName) {
+		this.nodeName = nodeName;
+	}
+	public String getOperationRequest() {
+		return operationRequest;
+	}
+	public void setOperationRequest(String operationRequest) {
+		this.operationRequest = operationRequest;
+	}
+	public String getOperationResult() {
+		return operationResult;
+	}
+	public void setOperationResult(String operationResult) {
+		this.operationResult = operationResult;
+	}
+	public String getOperationTimestamp() {
+		return operationTimestamp;
+	}
+	public void setOperationTimestamp(String operationTimestamp) {
+		this.operationTimestamp = operationTimestamp;
+	}
+
+
+	@Override
+	public String toString() {
+		return operationTimestamp + " [" + nodeName + "]: "+operationRequest + " = "+operationResult;
+	}
+
+
+	@Override
+	public int compareTo(ComputationRequestInfo arg0) {
+		return getOperationTimestamp().compareTo(arg0.getOperationTimestamp());
+	}
+	
+	
+	
+	
+	
+	
 }
